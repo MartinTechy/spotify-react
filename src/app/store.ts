1f6fc084a468666dc68e95e3ac704e36727b38ad
Martin Techy <martintechy@gmail.com>
2021-03-25 13:50:15 +0100

    Add redux-saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,22 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action, applyMiddleware } from '@reduxjs/toolkit';
+import createSagaMiddleware from "redux-saga"
+
+import sagas from './sagas'
+import reducer from './reducers'
+
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+const middelwareEnhancer = applyMiddleware(...middlewares);
+const enhancers = [middelwareEnhancer];
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  devTools: true,
+  reducer,
+  enhancers
 });
+
+sagaMiddleware.run(sagas)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<

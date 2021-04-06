e0190be0070e0d750fb09dc4afb5599adf38ffc0
Martin Techy <martintechy@gmail.com>
2021-04-07 00:53:26 +0200

    Style playlist details

--- src/features/track/TrackDetail.tsx
+++ src/features/track/TrackDetail.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import './TrackDetail.scss';
+
 import { Track } from './trackTypes';
 
 type Props = {
@@ -6,11 +9,27 @@ type Props = {
 }
 
 function TrackDetail({ track }: Props) {
-	const { name, album } = track;
+	const { name, album, artists } = track;
 
-	return (<>
-		{`${name} - ${album.name} - ${album.release_date}`}<br />
-	</>);
+	return (<div className='spot-track-detail'>
+		<div className='spot-track-detail__album-cover'>
+			<img src={album.images[0].url} alt='album cover' />
+		</div>
+		<div className='spot-track-detail__track-info'>
+			<div>
+				{name}
+			</div>
+			<div>
+				{artists.map(artist => artist.name).join(', ')}
+			</div>
+		</div>
+		<div className='spot-track-detail__album-name'>
+			{album.name}
+		</div>
+		<div className='spot-track-detail__release-date'>
+			{album.release_date}
+		</div>
+	</div>);
 }
 
 export default TrackDetail;
